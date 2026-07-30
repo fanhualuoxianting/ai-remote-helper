902b844becd63dade3785331f7569cd949f6bc5f
Zhu Hui <fanhualuoxianting@gmail.com>
2026-07-30 22:32:46 +0800

    Cover cloud credentials and env files

--- common-safety/src/test/java/com/airh/safety/SensitiveFileProtectorTest.java
+++ common-safety/src/test/java/com/airh/safety/SensitiveFileProtectorTest.java
@@ -14,6 +14,16 @@ class SensitiveFileProtectorTest {
     void blocksSshAndGpgDirectories() {
         assertEquals(RiskLevel.BLOCKED, protector.checkPath("~/.ssh/id_rsa"));
         assertEquals(RiskLevel.BLOCKED, protector.checkPath("/home/user/.gnupg/private-keys-v1.d/key"));
+        assertEquals(RiskLevel.BLOCKED, protector.checkPath("project/secrets/id_ed25519"));
+    }
+
+    @Test
+    void blocksCloudAndClusterCredentials() {
+        assertEquals(RiskLevel.BLOCKED, protector.checkPath("~/.aws/credentials"));
+        assertEquals(RiskLevel.BLOCKED, protector.checkPath("~/.azure/accessTokens.json"));
+        assertEquals(RiskLevel.BLOCKED, protector.checkPath("~/.kube/config"));
+        assertEquals(RiskLevel.BLOCKED,
+                protector.checkPath("~/.config/gcloud/application_default_credentials.json"));
     }
 
     @Test
@@ -41,6 +51,14 @@ class SensitiveFileProtectorTest {
         assertEquals(RiskLevel.HIGH, protector.checkPath("/etc/hosts"));
         assertEquals(RiskLevel.HIGH,
                 protector.checkPath("C:\\Users\\dev\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\helper.bat"));
+    }
+
+    @Test
+    void marksCommonProjectCredentialFilesAsHighRisk() {
+        assertEquals(RiskLevel.HIGH, protector.checkPath("project/.env"));
+        assertEquals(RiskLevel.HIGH, protector.checkPath("project/.npmrc"));
+        assertEquals(RiskLevel.HIGH, protector.checkPath("project/gradle.properties"));
+        assertEquals(RiskLevel.HIGH, protector.checkPath("project/.m2/settings.xml"));
     }
 
     @Test
